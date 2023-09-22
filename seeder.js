61f4a0757772061334806360e0a7b307303be0f1
Ali Raisi <aliraisi1376@gmail.com>
2023-09-22 17:30:55 +0330

    Added selection, sorting, pagination and course model, seeder file updated

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' })
 
 // Load the Models
 const Bootcamp = require('./models/Bootcamp')
+const Course = require('./models/Course')
 
 // Connect to DB
 mongoose.connect('mongodb://127.0.0.1:27017/devcamper')
@@ -16,11 +17,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/devcamper')
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 )
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+)
 
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
+    await Course.create(courses)
     console.log('Data imported...'.green.inverse)
     process.exit()
   } catch (err) {
@@ -32,6 +37,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany()
+    await Course.deleteMany()
     console.log('Data deleted...'.red.inverse)
     process.exit()
   } catch (err) {
