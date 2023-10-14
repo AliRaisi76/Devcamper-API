1c6ae62a49563acd92d44d139401053e58fe102f
Ali Raisi <aliraisi1376@gmail.com>
2023-10-14 21:41:21 +0330

    Added admin CRUD functionality

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const connectDB = require('./config/db')
 const bootcamps = require('./routes/bootcamps')
 const courses = require('./routes/courses')
 const auth = require('./routes/auth')
+const users = require('./routes/users')
 
 connectDB()
 
@@ -39,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
 app.use('/api/v1/auth', auth)
+app.use('/api/v1/users', users)
 
 // Use errorHandler as middelware
 app.use(errorHandler)
